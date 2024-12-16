6eda7e89a1368cd808107230e4c04444768b32ff
ningang <ningang@cabrtech.com>
2024-12-16 15:33:13 +0800

    feat：使用createRoot取代render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NodeProvider } from './context/NodeContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <NodeProvider>
       <App />
     </NodeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
